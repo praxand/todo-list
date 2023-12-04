ac3468995fe6cc14f255aec853181a548040ae85
praxand <jay.cheung2005@gmail.com>
2023-12-04 09:27:15 +0100

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,28 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/', [TaskController::class, 'index'])
+    ->name('tasks.index');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::post('/', [TaskController::class, 'store'])
+    ->name('tasks.store');
+
+Route::delete('/clear', [TaskController::class, 'clear'])
+    ->name('tasks.clear');
+
+Route::get('/{task}/edit', [TaskController::class, 'edit'])
+    ->name('tasks.edit');
+
+Route::put('/{task}/complete', [TaskController::class, 'complete'])
+    ->name('tasks.complete');
+
+Route::get('/{task}', [TaskController::class, 'show'])
+    ->name('tasks.show');
+
+Route::put('/{task}', [TaskController::class, 'update'])
+    ->name('tasks.update');
+    
+Route::delete('/{task}', [TaskController::class, 'destroy'])
+    ->name('tasks.destroy');
