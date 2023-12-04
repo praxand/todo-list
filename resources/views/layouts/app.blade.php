ab993d11d1592bfb9d9d1bd6be7efeb23f04c9ca
praxand <jay.cheung2005@gmail.com>
2023-12-04 14:47:38 +0100

    typo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
         @endif
 
         @if (session()->has('success'))
-            <x-notifgication
+            <x-notification
                 :message="session('success')"
                 title="Success!"
                 type="success"
